052b6a59716d46a841801d0dc7a79e3c21fc27c1
Amine BELLA <aminebella103112@gmail.com>
2023-09-17 14:06:18 +0100

    Implement Circuit Breaker - use case: microservice is down

--- order-service/src/main/java/com/project/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/project/orderservice/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.project.orderservice.controller;
 
 import com.project.orderservice.dto.OrderDto;
 import com.project.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -15,11 +16,17 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void placeOrder(@RequestBody OrderDto orderDto) throws Exception {
+    @CircuitBreaker(name="inventory", fallbackMethod = "placeOrderFallBack")
+    public String placeOrder(@RequestBody OrderDto orderDto) throws Exception {
         try {
             orderService.placeOrder(orderDto);
+            return "Order placed successfully!";
         } catch (Exception e) {
             throw new Exception(e.getMessage());
         }
+    }
+
+    public String placeOrderFallBack(OrderDto orderDto, RuntimeException runtimeException) {
+        return "System Error, please retry later!";
     }
 }
